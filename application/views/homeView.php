39e4fd4eda760934b63bfec4cf772165642d1225
Michel Leisibach <michel_leisibach@sluz.ch>
2019-11-13 09:01:25 +0100

    connected db

--- application/views/homeView.php
+++ application/views/homeView.php
@@ -1,14 +1,24 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="utf-8">
-	<title>hautbois Ltd.</title>
+    <meta charset="utf-8">
+    <title>hautbois Ltd.</title>
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-
-
+    <br>
+    <br>
+    <div class="container.fluid">
+    <div class="row">
+        <div class="col-4"> Test </div>
+        <div class="col-8"> Test </div>
+    </div>
+</div>
 </body>
+
 </html>
