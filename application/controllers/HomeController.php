90bedc058f44af9ccd4b1b8dd1e85253f014781f
Michel Leisibach <michel_leisibach@sluz.ch>
2019-11-13 08:22:42 +0100

    add stylesheet

--- application/controllers/HomeController.php
+++ application/controllers/HomeController.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class HomeController extends CI_Controller {
 	public function index()
 	{
-		$this->load->view('homeView');
+        $this->load->view('homeView');
+        $this->load->helper('url');
 	}
 }
--- application/views/homeView.php
+++ application/views/homeView.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="utf-8">
 	<title>hautbois Ltd.</title>
-
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/style.css">
 </head>
 <body>
 
