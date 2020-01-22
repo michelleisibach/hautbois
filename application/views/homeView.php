15df55987a82968d487f319baec8700e50d5e470
Michel Leisibach <michel.leisibach@gmail.com>
2020-01-22 08:34:33 +0100

    added favicon

--- application/views/homeView.php
+++ application/views/homeView.php
@@ -10,6 +10,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url(); ?>assets/pics/karikaturtiger-der-ein-oboe-vector-spielt-29806693 (1).ico">
 </head>
 
 <body style="background-color: #1C1C1C">
